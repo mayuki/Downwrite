b35e291529ca3159c1b62a6560210ef16365e40e
Mayuki Sawatari <mayuki@misuzilla.org>
2012-07-29 03:49:57 +0900

    文法の参考ヘルプを付けた。

--- Downwrite/Downwrite/js/default.js
+++ Downwrite/Downwrite/js/default.js
@@ -44,5 +44,10 @@
         app.sessionState.history = nav.history;
     };
 
+    app.onsettings = function (e) {
+        e.detail.applicationcommands = { "Help": { title: "Markdown Syntax", href: "/pages/flyoutHelp/flyoutHelp.html" } };
+        WinJS.UI.SettingsFlyout.populateSettings(e);
+    }
+
     app.start();
 })();
